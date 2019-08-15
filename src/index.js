6971644c9b619a37e5ad5335bea120113afdfeb5
Rakzhodekams <ofmz@autistici.org>
2019-08-15 21:54:19 +0100

    There are some errors in index.js and in main.js, while main.js code is working and index.js is not

--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
 /*jshint esversion: 6 */
 
-function socialmedia(id) {
-  const social = {
-    blogspot: 'https://www.facebook.com/odicforcesounds',
-    bsicon: 'img/icons8-blogger-48.png',
-    facebook: 'https://www.facebook.com/odicforcesounds',
-    github: 'https://github.com/rakzhodekams',
-    instagram: 'https://www.instagram.com/odicforcesounds/',
-    redit: 'https://www.reddit.com/user/odicforcesounds',
-    stackoverflow: 'https://stackoverflow.com/users/6806076/odicforcesounds',
-    tumblr: 'https://rakzhodekams.tumblr.com/',
-    twitter: 'https://twitter.com/odicforcesounds',
-    youtube: 'https://www.youtube.com/channel/UCKxb1p0OzXuiAQBkiY0dCGg',
-    yticon: '<img width="24px" src="./img/icons8-youtube-48.png"',
-  };
-    // simplifying Social Media Links Access
-  const bs = social.blogspot + social.bsicon;
-  const fb = social.facebook;
-  const gh = social.github;
-  const ins = social.instagram;
-  const red = social.redit;
-  const stof = social.stackoverflow;
-  const tum = social.tumblr;
-  const twit = social.twitter;
-  const tube = social.youtube + social.yticon;
-  // Some kind of 'hack'
-  // linkStart + tube + linkBehave + linkProps + linkContent + linkEnd
-  const linkStart = '<a href=';
-  const linkBehave = 'target=_blank>';
-  const linkProps = '<img width="24px"';
-  // const linkContent = '  ';
-  const linkEnd = '></a>';
-  const setupBSLink = `${linkStart + bs} ${linkBehave}${linkProps}${linkEnd}`;
-  const setupFBLink = linkStart + fb + linkBehave + linkProps  + linkEnd;
-    const setupGHLink = linkStart + gh + linkBehave + linkProps  + linkEnd;
-    const setupINSLink = linkStart + ins + linkBehave + linkProps + linkEnd;
-    const setupREDLink = linkStart + red + linkBehave + linkProps  + linkEnd;
-    const setupStackLink = linkStart + stof + linkBehave + linkProps + linkEnd;
-    const setupTUMLink = linkStart + tum +  linkBehave + linkProps + linkEnd;
-    const setupTWILink = linkStart + twit + linkBehave + linkProps + linkEnd;
- const setupYTILink = linkStart + tube+linkBehave$+linkProps+linkEnd;
-  // eslint-disable-next-line no-unused-consts
-  const injectAll = setupBSLink + setupYTILink;
 
-  // eslint-disable-next-line no-constant-condition
- 
-}socialmedia(); 
+const social = {
+    blogspot: '<a target="_blank" href="https://www.facebook.com/odicforcesounds"><img width="24px" src="./img/icons8-blogger-48.png"></a>',
+    facebook: '<a target="_blank" href="https://www.facebook.com/odicforcesounds"><img width="24px" src="./img/icons8-facebook-circled-48.png"></a>',
+    github: '<a target="_blank" href="https://github.com/rakzhodekams"><img width="24px" src="./img/icons8-github-48.png"></a>',
+    instagram: '<a target="_blank" href="https://www.instagram.com/odicforcesounds/"><img width="24px" src="./img/icons8-instagram-48.png"></a>',
+    stackoverflow: '<a target="_blank" href="https://stackoverflow.com/users/6806076/odicforcesounds"><img width="24px" src="./img/stack-overflow.png"></a>',
+    tumblr: '<a target="_blank" href="https://rakzhodekams.tumblr.com/"><img width="24px" src="./img/icons8-tumblr-48.png"></a>',
+    twitter: '<a target="_blank" href="https://twitter.com/odicforcesounds"><img width="24px" src="./img/icons8-twitter-circled-48.png"></a>',
+    youtube: '<a target="_blank" href="https://www.youtube.com/channel/UCKxb1p0OzXuiAQBkiY0dCGg"></a>',
+};
+
+  social.blogspot = document.getElementById("blogspot").innerHTML;
+  social.facebook = document.getElementById("facebook").innerHTML;
+  social.github = document.getElementById("github").innerHTML;
+  social.instagram = document.getElementById("instagram").innerHTML;
+  social.stackoverflow = document.getElementById("stackoverflow").innerHTML;
+  social.twitter = document.getElementById("twitter").innerHTML;
+  social.youtube = document.getElementById("youtube").innerHTML;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 /* eslint-disable eol-last */
+
 function changecolor(id) {
   if (true) {
     document.body.style.background = document.getElementById(id).innerHTML;
