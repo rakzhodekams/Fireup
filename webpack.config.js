ec94b17a6c426dbf0001a5f4ee59f38befd50892
Rakzhodekams <ofmz@autistici.org>
2019-08-15 01:15:33 +0100

    More config lines for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,62 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CopyPlugin([
+      { from: './public', to: './', ignore: './public/index.html' },
+    ]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true,
+    },
+  },
+  devServer: {
+    open: true,
   },
 };
