cb5168f2cda54339532b28c02e16fb188bf5b37b
Rakzhodekams <ofmz@autistici.org>
2019-08-18 07:08:38 +0100

    more fun data to play with

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,74 +1,81 @@
 const profile = {
+    realName: 'OscarFM',
     profileName: 'Rakzhodekams',
     profilePicture: '',
     profileSlogan: 'If Time is Build within Our Self\'s, then Time is our self\'s as result of Time it self',
     profileArtwork: 'OdicforceSounds',
     profileAlbums: [
         {
-            albumName: 'Visions of Thor - My Son',
+            albumName: 'Visions of Thor - Mỹ Sơn',
             albumImage: '',
             albumDescription: '',
             albumTracks: [
                 {
-                    trackName01: [''],
+                    trackName01: ['Emerge - Internal Upgrade'],
                     trackPlayer01: [''],
                     trackImage01: [''],
                     trackDescription01: ['']
                 },
                 {
-                    trackName02: [''],
+                    trackName02: ['Water Horses - Woman Freedom'],
                     trackPlayer02: [''],
                     trackImage02: [''],
                     trackDescription02: ['']
                 },
                 {
-                    trackName03: [''],
+                    trackName03: ['Pain vs Love'],
                     trackPlayer03: [''],
                     trackImage03: [''],
                     trackDescription03: ['']
                 },
                 {
-                    trackName04: [''],
+                    trackName04: ['Black Birds - Red Eyes'],
                     trackPlayer04: [''],
                     trackImage04: [''],
                     trackDescription04: ['']
                 },
                 {
-                    trackName05: [''],
+                    trackName05: ['Burned In The Hands Of Wind - Noisy Silence'],
                     trackPlayer05: [''],
                     trackImage05: [''],
                     trackDescription05: ['']
                 },
                 {
-                    trackName06: [''],
+                    trackName06: ['Summer Night - Rock and Roll '],
                     trackPlayer06: [''],
                     trackImage06: [''],
                     trackDescription06: ['']
                 },
                 {
-                    trackName07: [''],
+                    trackName07: ['V - Euclidean Space'],
                     trackPlayer07: [''],
                     trackImage07: [''],
                     trackDescription07: ['']
                 }, 
                 {
-                    trackName08: [''],
+                    trackName08: ['Invisible Frequency - You can change'],
                     trackPlayer08: [''],
                     trackImage08: [''],
                     trackDescription08: ['']
                 }, 
                 {
-                    trackName09: [''],
+                    trackName09: ['MA6NATA - EVIL INSIDE'],
                     trackPlayer09: [''],
                     trackImage09: [''],
                     trackDescription09: ['']
                 }, 
                 {
-                    trackName10: [''],
+                    trackName10: ['Shake - My Snakes'],
                     trackPlayer10: [''],
                     trackImage10: [''],
                     trackDescription10: ['']
-                },                                                                                                                                
+                },
+                {
+                    trackName11: ['We Are The Ones'],
+                    trackPlayer11: [''],
+                    trackImage11: [''],
+                    trackDescription11: ['']
+                },                                                                                                                                                
             ]
         },
         {
