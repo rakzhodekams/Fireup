1a66290037e76fd6c75890c70855053713761462
Rakzhodekams <ofmz@autistici.org>
2019-08-30 00:11:49 +0100

    Stage 01 (Baby Steps) of 10000000000000000000000000000000000000000000000 ** 10000000000000000000000000000000000000000000000000000 ( lol ) Enjoy!

--- docs/index.js
+++ docs/index.js
@@ -1,37 +1,3 @@
 // const babel = require("@babel/core");
 // babel.transform("code", optionsObject);
-
-function blog(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Blog Top-menu </p>';
-}
-
-function code(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Code Top-menu <p>';
-}
-
-function de(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in <b>De</b> Top-menu</p>';
-}
-
-function eg(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in the Eg Top-menu</p>';
-}
-
-function fun(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Fun Top-menu</p>';
-}
-
-function game(){
-  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Game Top-menu</p>';
-}
-
-function music(){
-  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Music Top-menu</p>';
-}
-
-
-function wiki(){
-  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Wiki Top-menu</p>';
-  document.querySelector('.postContent').innerHTML = '<p> LoL <b> this should have wiki content </p>';
-}
-
+document.querySelector('.slogan').innerHTML = '<span>If Time is Build within Our Selfs, then Time is Our Selfs as result of Time it Self</span>';
