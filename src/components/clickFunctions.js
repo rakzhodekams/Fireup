2403a87f7604ad4188ccab3aef3b6e41e43009ca
Rakzhodekams <ofmz@autistici.org>
2019-08-30 00:09:38 +0100

    Almost Ready to Start Growing like a Baby :P

---
+++ src/components/clickFunctions.js
@@ -0,0 +1,44 @@
+// const babel = require("@babel/core");
+// babel.transform("code", optionsObject);
+
+function blog(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Blog Top-menu </p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Blogs Content</p>';
+}
+
+function code(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Code Top-menu <p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Code Content</p>';
+}
+
+function de(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in <b>De</b> Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have De Content</p>';
+}
+
+function eg(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in the Eg Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Eg Content</p>';
+}
+
+function fun(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Fun Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Fun Content</p>';
+}
+
+function game(){
+  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Game Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Game Content</p>';
+}
+
+function music(){
+  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Music Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LooL this should have Music Content</p>';
+}
+
+
+function wiki(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Wiki Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LoL <b> this should have wiki content </p>';
+}
+
