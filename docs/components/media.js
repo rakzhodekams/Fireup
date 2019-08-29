15e1e9150941622d7afa208a7d8024ffb96a01ee
Rakzhodekams <ofmz@autistici.org>
2019-08-29 17:32:38 +0100

    Make it public

---
+++ docs/components/media.js
@@ -0,0 +1,14 @@
+class bandcampUrlMap = {
+    siteroot: 'https://bandcamp.com/',
+    player: 'EmbeddedPlayer/',
+    size: '',
+    bgcolor: '',
+    linkcolor: '',
+    transparent: '', // True or False
+    // Non-Global Data
+    // AlbumID
+    albums = [
+        albumID: '3889959682',
+        trackID: '475476620',
+    ]
+}
--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,37 @@
-class bandcampUrlMap = {
-    siteroot: 'https://bandcamp.com/',
-    player: 'EmbeddedPlayer/',
-    size: '',
-    bgcolor: '',
-    linkcolor: '',
-    transparent: '', // True or False
-    // Non-Global Data
-    // AlbumID
-    albums = [
-        albumID: '3889959682',
-        trackID: '475476620',
-    ]
+// const babel = require("@babel/core");
+// babel.transform("code", optionsObject);
+
+function blog(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Blog Top-menu </p>';
 }
+
+function code(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Code Top-menu <p>';
+}
+
+function de(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in <b>De</b> Top-menu</p>';
+}
+
+function eg(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in the Eg Top-menu</p>';
+}
+
+function fun(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Fun Top-menu</p>';
+}
+
+function game(){
+  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Game Top-menu</p>';
+}
+
+function music(){
+  document.querySelector('.postsTitle').innerHTML =  '<p> You clicked in Music Top-menu</p>';
+}
+
+
+function wiki(){
+  document.querySelector('.postsTitle').innerHTML = '<p> You clicked in Wiki Top-menu</p>';
+  document.querySelector('.postContent').innerHTML = '<p> LoL <b> this should have wiki content </p>';
+}
+
